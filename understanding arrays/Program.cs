662bd22ee72e08ffea38614288b23920da8368b6
wambui7 <shiko.sheila@gmail.com>
2018-07-28 21:45:43 -0500

    commit all

--- understanding arrays/Program.cs
+++ understanding arrays/Program.cs
@@ -6,7 +6,55 @@ namespace understanding_arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            /*
+             int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+
+            Console.ReadLine();
+              */
+
+
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            
+            string[] names = new string[] { "eddie", "alex", "michael", "david lee" };
+            /*
+          for (int i = 0; i < names.Length; i++)
+          {
+              Console.WriteLine(names[i]);
+          }
+          Console.ReadLine();
+          */
+            /*
+              foreach (string name in names)
+              {
+                  Console.WriteLine(name);
+              }
+              Console.ReadLine();
+              */
+
+            string zig = "you can get what you want out of life" + "if you help enough other people get what they want.";
+
+            char[] chaArray = zig.ToCharArray();
+            Array.Reverse(chaArray);
+
+            foreach (char zigChar in chaArray)
+            {
+                Console.Write(zigChar);
+
+
+            }
+            Console.ReadLine();
         }
     }
 }
