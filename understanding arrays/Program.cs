080791f9cfe495d28062d1614e41d785d4a979a0
wambui7 <shiko.sheila@gmail.com>
2018-07-28 19:24:44 -0500

    Add project files.

---
+++ understanding arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace understanding_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
